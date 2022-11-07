4e99d815adb1558c8ea765d67f52e1a817ff3f50
Denys Samoilenko <56838967+denniksam@users.noreply.github.com>
2022-11-07 17:38:58 +0200

    Initial commit

---
+++ Intro/Assets/Scripts/Diamond.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamond : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.  // оскільки скрипт знаходиться на GameObject-і, this - GameObject(Diamond)
+            transform.  // доступ до компонента Transform
+            Rotate(Vector3.forward, 1);  // обертання на 1 градус з кожним фреймом
+
+    }
+    /* Д.З. Встановити та налагодити Unity, VS (VS Code, Rider)
+     * додати на сцену спрайт (довільної форми)
+     * скласти скрипт, що його обертає у іншу сторону, ніж попередні об'єкти
+     */
+}
