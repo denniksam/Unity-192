7b130df078b8402cda507031353b2d8694ab72e9
Denys Samoilenko <denniksam@gmail.com>
2022-11-16 13:32:39 +0200

    Menu -- 2

--- Intro/Assets/Scripts/GameMenu.cs
+++ Intro/Assets/Scripts/GameMenu.cs
@@ -7,9 +7,12 @@ public class GameMenu : MonoBehaviour
     [SerializeField]
     private GameObject MenuContainer;
 
+    [SerializeField]
+    private TMPro.TextMeshProUGUI MenuButtonText;
+
     void Start()
     {
-        
+        if (MenuContainer.activeInHierarchy) Time.timeScale = 0;
     }
 
     void Update()
@@ -29,3 +32,13 @@ public class GameMenu : MonoBehaviour
         }
     }
 }
+/* Time.timeScale - змінює хід часу.  
+ *  1 - нормальний хід
+ *  0 - зупинка часу (всі фізичні процеси та Time.deltaTime множаться на 0)
+ *  0..1 - "повільне програвання"
+ *  >1 - прискорене програвання
+ *  
+ *  !! 0 - не зупиняє скриптинг, всі Update виконуються, якщо у них
+ *         є геометричні перетворення, які не залежать від Time.deltaTime,
+ *         то вони продовжують виконуватись
+ */
